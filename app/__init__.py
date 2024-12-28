33c1d7337c5bf1f41ffe815285bd9f3510822aec
karanT <2001karanpt@gmail.com>
2024-12-28 20:24:49 +0530

    pushing changes for render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect
 import os
 
 def create_app():
-    app = Flask(__name__, template_folder='../templates')  # Explicit path
+    app = Flask(__name__, template_folder='../templates')
     app.config['SECRET_KEY'] = 'supersecretkey'
+
+    # HTTPS Redirect in Production
+    @app.before_request
+    def enforce_https():
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            return redirect(request.url.replace("http://", "https://"))
+
+    # Health Check Route
+    @app.route('/')
+    def health_check():
+        return jsonify({"message": "Backend is up and running!"}), 200
 
     from .routes import scraper_bp
     app.register_blueprint(scraper_bp, url_prefix='/')
